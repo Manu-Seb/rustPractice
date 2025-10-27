3044b5ad967ff984943f7a7d553486d79d8a1529
Manu-Seb <manusebastian018@gmail.com>
2025-10-27 20:38:26 +0530

    guessing game improved

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,20 +1,27 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
-    println!("Please enter a number between 1 and 10!");
-    let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess)
-        .expect("an error occurred");
-
-    let guess : u32 = guess.trim().parse().expect("an error occurred");
-
     let secret = rand::thread_rng().gen_range(1..=10);
-    match guess.cmp(&secret){
-        Ordering::Less => println!("You have guessed too little"),
-        Ordering::Greater=> println!("You have guessed too large"),
-        Ordering::Equal=> println!("You win"),
+    loop {
+        println!("Please enter a number between 1 and 10!");
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("an error occurred");
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("You have guessed too little"),
+            Ordering::Greater => println!("You have guessed too large"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
     }
 }
