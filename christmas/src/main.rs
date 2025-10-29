298398f0942c83a526519d2fdf31c2ddeadab01f
Manu-Seb <manusebastian018@gmail.com>
2025-10-29 16:48:36 +0530

    simple string slice func

--- christmas/src/main.rs
+++ christmas/src/main.rs
@@ -2,36 +2,16 @@ fn main() {
     let arr = [
         "And a partridge in a pear tree",
         "Two turtle doves",
-        "
-        Twelve drummers drumming
-        ",
-        "
-        Eleven pipers piping
-        ",
-        "
-        Ten lords a-leaping
-        ",
-        "
-        Nine ladies dancing
-        ",
-        "
-        Eight maids a-milking
-        ",
-        "
-        Seven swans a-swimming
-        ",
-        "
-        Six geese a-laying
-        ",
-        "
-        Five golden rings
-        ",
-        "
-        Four calling birds
-        ",
-        "
-        Three French hens
-        ",
+        "Twelve drummers drumming",
+        "Eleven pipers piping",
+        "Ten lords a-leaping",
+        "Nine ladies dancing",
+        "Eight maids a-milking",
+        "Seven swans a-swimming",
+        "Six geese a-laying",
+        "Five golden rings",
+        "Four calling birds",
+        "Three French hens",
     ];
 
     for i in 1..=12 {
@@ -40,5 +20,6 @@ fn main() {
         for j in 0..i {
             println!("{}", arr[j]);
         }
+        println!("");
     }
 }
---
+++ first_word/src/main.rs
@@ -0,0 +1,22 @@
+use std::io;
+fn main() {
+    let mut s = String::new();
+    println!("Please type your string");
+
+    io::stdin().read_line(&mut s).expect("Could not read line");
+
+    let word = find_first_word(&s);
+
+    println!("{word} is the first word");
+}
+
+fn find_first_word(s: &String) -> &str {
+    let items = s.as_bytes();
+
+    for (i, &item) in items.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+    return &s[..s.len()];
+}
